e0a4a0830519248e973cfeac12ee99b805ab6b1e
Phillip Lamplugh <phillip.lamplugh@gmail.com>
2013-07-16 11:04:10 -0400

    spell wizardry

--- app/controllers/spells_controller.rb
+++ app/controllers/spells_controller.rb
@@ -4,15 +4,27 @@ class SpellsController < ApplicationController
   end
 
   def new
+    @spell = Spell.new
+  end
+
+  def edit
+    @spell = Spell.find(params[:id])
   end
 
   def create
-    @spell = Spell.new(params[:name])
+    binding.pry
+    @spell = Spell.new(params[:spell])
+
     if @spell.save
       redirect_to @spell
     else
       render :new
     end
+  end
+
+  def update
+    binding.pry
+    @spell = Spell.find(params[:id])
   end
 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: students
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  house_id   :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Student < ActiveRecord::Base
   attr_accessible :name, :house_id, :spell_ids
   belongs_to :house
